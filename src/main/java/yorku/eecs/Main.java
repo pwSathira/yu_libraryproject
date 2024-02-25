812d1317454735ecf9f466a36c961664c982c6d4
Akul Sareen <sareen.akul@gmail.com>
2024-02-25 14:12:58 -0500

    Akul's Branch

--- src/main/java/yorku/eecs/Main.java
+++ src/main/java/yorku/eecs/Main.java
@@ -3,6 +3,6 @@ package yorku.eecs;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //sathira's branch
+        //Akul's Branch
     }
 }
