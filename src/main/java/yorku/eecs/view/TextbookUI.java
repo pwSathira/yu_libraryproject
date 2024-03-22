29c6f2dde718514c6554a50ae3a29526e1795ca0
Vinayak Passey <vinayak.passey@gmail.com>
2024-03-21 21:30:13 -0400

    Changes by Isaiah

--- src/main/java/yorku/eecs/view/TextbookUI.java
+++ src/main/java/yorku/eecs/view/TextbookUI.java
@@ -6,10 +6,18 @@ import yorku.eecs.controller.card.ViewSwitcher;
 import java.awt.*;
 
 public class TextbookUI extends HomeBaseUI {
+
+    protected ViewSwitcher viewSwitcher;
+    private JFXPanel jfxPanel;
+    private String path = "src/main/resources/data/newsletterdata.csv";
+    
     public TextbookUI(ViewSwitcher viewSwitcher) {
         super(viewSwitcher);
         jfxPanel = new JFXPanel();
         initializeUI();
         contentPanel.add(jfxPanel, BorderLayout.CENTER);
+    }
+
+    private void initializeUI() {
     }
 }
